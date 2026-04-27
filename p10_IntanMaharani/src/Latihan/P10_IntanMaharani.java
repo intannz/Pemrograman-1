23baaa90202f96c6ef5167abafddd5cea8279895
Intan Maharani <carimzar1943@gmail.com>
2026-04-27 09:02:09 +0700

    Pertemuan 10

---
+++ p10_IntanMaharani/src/Latihan/P10_IntanMaharani.java
@@ -0,0 +1,13 @@
+package Latihan;
+
+public class P10_IntanMaharani {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama;
+        int usia;
+    }
+    
+}
